405a4ecd54629b72f4d724cf62f9e371002672f4
wangruiCoder <893652901@qq.com>
2019-03-02 22:27:21 +0800

    完善 jjwt 工具类

--- src/main/java/cn/newstrength/core/encryption/jwt/JwtHelpor.java
+++ src/main/java/cn/newstrength/core/encryption/jwt/JwtHelpor.java
@@ -37,7 +37,7 @@ public class JwtHelpor {
     }
 
     /**
-     * 生成token
+     * 通过设置 payload 生成token,具体的payload信息请参考payload标准
      * @param payload 存入token的信息模块，信息按照json格式传入<br/>
      * payload 标准字段有
      * <ul>
@@ -65,7 +65,8 @@ public class JwtHelpor {
     /**
      * 通过设置claimMaps生成token,claimMaps中切勿存放敏感信息和过量信息,防止生成的token超过4KB,超过cookie可以支持的最大长度。
      * @param claimMaps 需要放入token中的信息
-     * @param signatureAlgorithm 采用的数据加密算法，此算法设置完毕后会应用到签名key和token的生成
+     * @param signatureAlgorithm 采用的数据加密算法，此算法设置完毕后会应用到签名key和token的生成，
+     *                           如果这个值为<i>SignatureAlgorithm.NONE<i/>，系统会自动设定其为<i>SignatureAlgorithm.HS256<i/><br/>
      * @param issuedAt token的起始时间，请使用系统当前时间
      * @param calendarFieldEnum 生成token时可支持的时间单位:分钟,小时,天
      * @param calendarInterval token有效时间
@@ -76,6 +77,7 @@ public class JwtHelpor {
     public String createToken(Map<String,Object> claimMaps, SignatureAlgorithm signatureAlgorithm,
                               Date issuedAt, CalendarFieldEnum calendarFieldEnum, int calendarInterval) throws RuntimeException{
 
+        // 判定设置的token时间是否在当前枚举中要求的时间段范围之内
         if(calendarInterval < calendarFieldEnum.getMinValue()
                 || calendarInterval > calendarFieldEnum.getMaxValue()){
             throw new IllegalArgumentException("过期时间设置异常，请输入介于"+calendarFieldEnum.getMinValue()+"~"+calendarFieldEnum.getMaxValue()+"正整数");
@@ -92,16 +94,48 @@ public class JwtHelpor {
         return token;
     }
 
-    public boolean verifyToken(String token, SignatureAlgorithm signatureAlgorithm){
+    /**
+     * 验证token是否已经过期
+     * @param token token
+     * @param signatureAlgorithm token加密算法
+     * @return 过期结果,true 过期,false 未过期
+     */
+    public boolean verifyTokenExpired(String token, SignatureAlgorithm signatureAlgorithm){
+        boolean expired = false;
+
+        long currentTime = new Date().getTime();
         initSignatureAlgorithm(signatureAlgorithm);
 
         Jws<Claims> claimsJws = Jwts.parser().setSigningKey(getKey()).parseClaimsJws(token);
-        JwsHeader header = claimsJws.getHeader();
         Claims body = claimsJws.getBody();
-        System.out.println(body.get("exp"));
-        return true;
+        long expTime =  (long) body.get("exp");
+
+        if(currentTime > expTime){
+            expired = true;
+        }
+
+        return expired;
     }
 
+    /**
+     * 获取token的主体
+     * @param token token
+     * @param signatureAlgorithm token加密算法
+     * @return token主体
+     */
+    public Map<String,Object> getTokenBody(String token, SignatureAlgorithm signatureAlgorithm){
+        initSignatureAlgorithm(signatureAlgorithm);
+
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(getKey()).parseClaimsJws(token);
+        Claims body = claimsJws.getBody();
+
+        return body;
+    }
+
+    /**
+     * 签名加密
+     * @return 加密后的签名串
+     */
     private Key getKey(){
 
         return new SecretKeySpec(SECRET.getBytes(),
